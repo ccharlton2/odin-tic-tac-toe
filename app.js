8954c9aa2c800e81ee78198fd93b1d044d137494
Christian <christiancharlton17@gmail.com>
2023-02-24 11:49:56 -0700

    Track current player

--- app.js
+++ app.js
@@ -8,34 +8,48 @@ const Player = (name, isHuman, token) => {
   return { getName, getToken, getIsHuman };
 };
 
-const Game = ((playerOne, playerTwo) => {
+const Game = (() => {
   const gameBoard = [
-    ["O", "X", "X"],
-    ["X", "O", "X"],
-    ["X", "X", "O"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
+
+  const playerOne = Player("Cross", true, "X");
+  const playerTwo = Player("Naught", true, "O");
+  let playerTurn = playerOne;
 
   const DisplayController = (() => {
     const render = () => {
-      const rowContainer = document.querySelector(".board-container");
+      const boardContainer = document.querySelector(".board-container");
+      boardContainer.textContent = "";
+      const playerTurnDiv = document.querySelector(".current-turn");
+      playerTurnDiv.textContent = `Who's turn is it anyway? ${getPlayerTurn().getToken()}`;
 
       for (let rowIndex = 0; rowIndex < gameBoard.length; rowIndex += 1) {
         const newRow = document.createElement("div");
-        newRow.classList.add("row");
-        console.log(newRow);
-
+        newRow.classList.add(`row-${rowIndex}`);
         for (
           let colIndex = 0;
           colIndex < gameBoard[rowIndex].length;
           colIndex += 1
         ) {
           const newCol = document.createElement("div");
-          newCol.classList.add("col");
+          newCol.classList.add(`col-${colIndex}`);
           newCol.textContent = gameBoard[rowIndex][colIndex];
+          newCol.addEventListener("click", () => {
+            const whosTurn = getPlayerTurn();
+            placeToken(
+              whosTurn === playerOne
+                ? playerOne.getToken()
+                : playerTwo.getToken(),
+              rowIndex,
+              colIndex
+            );
+          });
           newRow.appendChild(newCol);
         }
-
-        rowContainer.appendChild(newRow);
+        boardContainer.appendChild(newRow);
       }
     };
     return { render };
@@ -45,15 +59,27 @@ const Game = ((playerOne, playerTwo) => {
 
   const placeToken = (token, x, y) => {
     gameBoard[x][y] = token;
+    playerTurn = playerTurn === playerOne ? playerTwo : playerOne;
+    controller.render();
   };
 
   const getGameBoard = () => gameBoard;
 
   const getController = () => controller;
 
-  return { getGameBoard, placeToken, getController };
+  const getPlayers = function () {
+    return { playerOne, playerTwo };
+  };
+
+  const getPlayerTurn = () => playerTurn;
+
+  return { getGameBoard, placeToken, getController, getPlayers };
 })();
 
 const newGame = Game;
 
 newGame.getController().render();
+
+const players = newGame.getPlayers();
+
+console.log(players.playerOne.getName());
